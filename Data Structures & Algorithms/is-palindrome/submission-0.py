8d87165cea1fc52ff0be77a33b90504898df3fce
Om Govind Jha <130828932+OmGovind15@users.noreply.github.com>
2026-09-11 23:28:17 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.py
@@ -0,0 +1,49 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s=s.lower()
+        #print(s)
+        p=[]
+        for i in s:
+            if i.isalnum():
+                p.append(i)
+        print(p)
+        x=0
+        y=len(p)-1
+        r=False
+        if len(p)%2!=0:
+            c=0
+            while(x<len(p)//2 and y>len(p)//2):
+
+                #print('x',x)
+                
+                #print('y',y)
+                #print('value_X',p[x])
+                #print('value_Y',p[y])
+
+                if(p[x]==p[y]):
+                    c=c+1
+                    print('xx',x)
+                    print('yy',y)
+                    print('cc',c)
+                x=x+1
+                y=y-1
+            if(c==len(p)//2):
+                r=True
+            print('c',c)
+        m=0
+        n=len(p)
+        if len(p)%2==0:
+            d=0
+            while(m<len(p)//2 and n>=len(p)//2):
+                print('m',x)
+                print('n',y)
+                if(p[m]==p[n]):
+                    c=c+1
+                m=m+1
+                n=n-1
+            print('d',d)
+            if(d==len(p)//2):
+                r=True
+        return r
+
+
